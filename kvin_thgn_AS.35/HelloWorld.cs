c4b2f662bbc6db6038a853f7ccb7becf0e066ef2
z3ss <kmn1993@gmail.com>
2014-09-02 14:27:46 +0200

    Added AS 35

---
+++ kvin_thgn_AS.35/HelloWorld.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class HelloWorld
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int count = 0;
+            foreach (var arg in args)
+            {
+                count++;
+                Console.WriteLine(arg);
+            }
+            Console.WriteLine("Number of arguments: "+count);
+        }
+    }
+}
