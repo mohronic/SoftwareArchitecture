165de21021aeb049e21d607bdb28fd0d07facdc8
Tróndur Høgnason <thgn.itu.dk>
2014-09-30 16:41:55 +0200

    Various

--- Calendar_System/Calendar_System/control/AccountControl.cs
+++ Calendar_System/Calendar_System/control/AccountControl.cs
@@ -3,11 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Calendar_System.model;
+using Calendar_System.view;
 
 namespace Calendar_System.control
 {
     class AccountControl
     {
+        public void CreateAccount()
+        {
+            var accountForm = new AccountForm();
+        }
 
+        public void ModifyAccount(User user)
+        {
+            var accountForm = new AccountForm(user);
+        }
+
+        public void DeleteAccount()
+        {
+            
+        }
     }
 }
--- Calendar_System/Calendar_System/view/WorkgroupForm.cs
+++ Calendar_System/Calendar_System/view/WorkgroupForm.cs
@@ -3,10 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Calendar_System.model;
 
 namespace Calendar_System.view
 {
     class WorkgroupForm
     {
+        public WorkgroupForm()
+        {
+            
+        }
+
+        public WorkgroupForm(Workgroup workgroup)
+        {
+            
+        }
     }
 }
