1268c93ba94107bb0b2ca3ccfbbbec3b6085bd3d
z3ss <kmn1993@gmail.com>
2014-09-30 16:16:44 +0200

    updates to storage

---
+++ Calendar_System/Calendar_System/model/AbstractStorage.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Calendar_System.Utility;
+
+namespace Calendar_System.model
+{
+    abstract class AbstractStorage : Observable
+    {
+        public abstract List<Entry> GetEntries();
+        public abstract void UpdateEntry(Entry entry);
+        public abstract void CreateEntry(Entry entry);
+        public abstract void CreateUser(User user);
+        public abstract List<User> GetUsers();
+        public abstract void UpdateUser(User user);
+        public abstract void CreateWorkgroup(Workgroup wg);
+        public abstract List<Workgroup> GetWorkgroups();
+        public abstract void DeleteWorkgroup(Workgroup wg);
+    }
+}
--- Calendar_System/Calendar_System/model/StorageImp.cs
+++ Calendar_System/Calendar_System/model/StorageImp.cs
@@ -6,7 +6,56 @@ using System.Threading.Tasks;
 
 namespace Calendar_System.model
 {
-    class StorageImp
+    class StorageImp : AbstractStorage
     {
+        public StorageImp()
+        {
+            
+        }
+
+        public override List<Entry> GetEntries()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void UpdateEntry(Entry entry)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void CreateEntry(Entry entry)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void CreateUser(User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override List<User> GetUsers()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void UpdateUser(User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void CreateWorkgroup(Workgroup wg)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override List<Workgroup> GetWorkgroups()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void DeleteWorkgroup(Workgroup wg)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
