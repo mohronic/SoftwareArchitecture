2072e6d8e7f7cbd4f88833d5121747e5301e49e6
Tróndur Høgnason <thgn.itu.dk>
2014-10-02 11:51:45 +0200

    og lidt info til dig og TAs

--- Calendar_System/Calendar_System/Program.cs
+++ Calendar_System/Calendar_System/Program.cs
@@ -6,12 +6,13 @@ namespace Calendar_System
     class Program
     {
         // If you want to run as user in current implementation
-        static void Main2()
+        // Username is Hans (case sensitive!) and password is 12345.
+        static void Main()
         {
             CalendarControl cControl = new CalendarControl();
         }
         // If you want to run as admin in current implementation
-        static void Main()
+        static void Main2()
         {
             AdminControl ac = new AdminControl();
         }
